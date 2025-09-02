53600041bf2eff394f519cfedc8fccb8e56f1a1e
jo9oct <yohannishailye@gmail.com>
2025-09-03 00:17:31 +0300

    d

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 
 app.use(
     cors({
-        origin: "http://localhost:5173" || "*",
+        origin: "https://hotel-menu-32.onrender.com",
         credentials: true,
     })
 );
