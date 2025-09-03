9e308f63156a26e91ea4c7f0b2814b039cbd4d1a
YOHANNIS <166551818+jo9oct@users.noreply.github.com>
2025-09-03 15:00:20 +0300

    Update generateTokenAndSetCookie.js

--- src/utils/generateTokenAndSetCookie.js
+++ src/utils/generateTokenAndSetCookie.js
@@ -5,7 +5,7 @@ export const generateTokenAndSetCookie= (res,userID) =>{
     const token=JWT.sign({userID} , process.env.JWT_SECRET , {
         expiresIn:"7d",
     })
-
+ console.log("token" , token)
     res.cookie("token" , token , {
         httpOnly:true,
         secure: process.env.NODE_ENV === "production",
