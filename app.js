0d0f05f40a9265e4fe25a4cf3e6a54e5d868ac69
jo9oct <yohannishailye@gmail.com>
2025-09-03 13:15:57 +0300

    g

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ import orderRoute from "./src/router/order.route.js"
 import cors from "cors"
 import AuthRouter from "./src/router/auth.route.js"
 import cookieParser from "cookie-parser";
-import path from "path"
 
 dotenv.config()
 
@@ -15,14 +14,13 @@ const app=express()
 
 await connectDB()
 const PORT=process.env.PORT || 5721
-const __dirname = path.resolve();
 
 app.use(express.json())
 app.use(cookieParser())
 
 app.use(
     cors({
-        origin: "https://hotel-menu-32.onrender.com",
+        origin: "https://hotel-front-pvvv.vercel.app",
         credentials: true,
     })
 );
@@ -31,12 +29,6 @@ app.use("/api/auth" , AuthRouter)
 app.use("/api/food" , foodRoute)
 app.use("/api/order" , orderRoute)
 
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// SPA fallback
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
 
 app.listen(PORT , () => {
     console.log(`My Hotel Menu Link http://localhost:${PORT}`)
